94b992a4106bb897fb3f7e5d6da8e9276795895e
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-11-18 17:49:40 +0200

    done

--- question1.py
+++ question1.py
@@ -5,8 +5,10 @@ class Grade():
         self.student_id = student_id
         self.grade = grade
 
+
 def __str__(self):
     return f'Hello student: {self.student_id}, {self.id}/t your grade in {self.subject_name} is:{self.grade}'
+
 
 def __repr__(self):
     return f'Grade(id = {self.id}, ' \
@@ -14,37 +16,45 @@ def __repr__(self):
            f'subject_name = {self.subject_name})' \
            f'grade = {self.grade}'
 
+
 def __eq__(self, other):
     if isinstance(other, Grade):
         return self.grade == other.grade
     elif type(other) in [str]:
         return False
 
-def __ne__(self,other):
+
+def __ne__(self, other):
     return not self.__eq__(other)
 
-def __gt__(self,other):
+
+def __gt__(self, other):
     if isinstance(other, Grade):
         return self.grade > other.grade
+
+
 #    elif type(other) in [int, float]:
 #        return self.account_id > other
 #    else:
 #        # throw Error
 #        return False
 
-def __lt__(self,other):
+def __lt__(self, other):
     if isinstance(other, Grade):
         return self.grade < other.grade
+
+
 #    elif type(other) in [int, float]:
 #        return self.account_id < other
 #    else:
 #        # throw Error
 #        return False
 
-def __add__(self,other):
+def __add__(self, other):
     self.grade += other
     return self
 
-def __sub__(self,other):
+
+def __sub__(self, other):
     self.grade -= other
     return self
---
+++ shelve.py
@@ -0,0 +1,6 @@
+from question1 import Grade
+
+lizas_grade = Grade( 123, 'Math', 55555, 84)
+mays_grade = Grade(124, 'Art', 25413, 100)
+rinas_grade = Grade(125, 'Chemistry', 88569, 56)
+irenas_grade = Grade(126, '')
